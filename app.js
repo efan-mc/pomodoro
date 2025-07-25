f8a6585b997b1c4acec1727a6fd34c4732d0046f
efan-mc <efangeorge@gmail.com>
2025-07-25 16:12:05 +0100

    Added sound for bell and initialise variables in app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const bells = new Audio('./sounds/bells.wav');
+const startBtn = document.querySelector('.btn-start');
+const session = document.querySelector('.minutes');
+let myInterval;
+let state = true;
+
